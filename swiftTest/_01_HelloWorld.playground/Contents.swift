07bf4e6a20ab17434148b4ea7729fbca89a5a054
吴贞利 <wuzhenli@kongfz.com>
2017-03-09 21:53:47 +0800

    1、swift基础语法

--- swiftTest/_01_HelloWorld.playground/Contents.swift
+++ swiftTest/_01_HelloWorld.playground/Contents.swift
@@ -4,12 +4,50 @@ import UIKit
 
 var str = "Hello, playground"
 
-enum VendingMachineError: Error {
-    case invalidSelection                    //选择无效
-    case insufficientFunds(coinsNeeded: Int) //金额不足
-    case outOfStock                          //缺货
-}
+var i: Int = 1
+var i_1: Int16 = 2
+var i_2: Int32 = 3
 
-throw VendingMachineError.insufficientFunds(coinsNeeded: 23)
+var b: Bool = false
+
+let httpError = (404, "not found")
+
+let (code, msg) = httpError
+print(httpError.0)
+
+
+let str1: String? = "may be nil"
+let str2: String = str1! // 加 ！ 获取值
+
+let str3: String = "man be nil alse"
+let str4: String = str3
+
+
+var str_char = "chartes"
+str_char += "ddd"
+//for c in str_char.characters {
+//    print(c)
+//}
+
+// MARK : http://wiki.jikexueyuan.com/project/swift/chapter2/07_Closures.html
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
