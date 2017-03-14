26ae9bebce26f15bcf306d8b862d5d14c67e00c7
吴贞利 <wuzhenli@kongfz.com>
2017-03-14 16:17:13 +0800

    1、Layer动画暂停、开始 2、layer层动画

--- animation/animation/LayerAnimationViewController.m
+++ animation/animation/LayerAnimationViewController.m
@@ -10,14 +10,25 @@
  */
 
 #import "LayerAnimationViewController.h"
-#import <QuartzCore/QuartzCore.h>b
+#import <QuartzCore/QuartzCore.h>
+// 104  157
+
+static CGFloat const kImgViewWidth = 104;
+static CGFloat const kImgViewHeight = 157;
 
 
-@interface LayerAnimationViewController ()
-@property (strong, nonatomic) CAShapeLayer *shapLayer;
-@property (weak, nonatomic) IBOutlet UIButton *greenView;
+@interface LayerAnimationViewController () {
+    BOOL isAnimation;
+}
+@property (strong, nonatomic) CAShapeLayer *shapLayer; // 顶部暂停动画的layer
+@property (strong, nonatomic) CAShapeLayer *redLabelLayer; //
+
+@property (weak, nonatomic) IBOutlet UIImageView *topImgView;
+@property (weak, nonatomic) IBOutlet UIImageView *bottomImgView;
 
 @property (weak, nonatomic) IBOutlet UILabel *redLabel;
+
+@property (weak, nonatomic) IBOutlet UISlider *slider;
 
 @end
 
@@ -26,91 +37,124 @@
 - (CAShapeLayer *)shapLayer {
     if (!_shapLayer) {
         _shapLayer = [CAShapeLayer layer];
-        _shapLayer.frame = CGRectMake(0, 66, 100, 100);
-        _shapLayer.position = CGPointMake(10, 70);
-        _shapLayer.bounds = CGRectMake(0, 0, 100, 100);
-        _shapLayer.backgroundColor = [UIColor whiteColor].CGColor;
     }
     return _shapLayer;
 }
 
+- (CAShapeLayer *)redLabelLayer {
+    if (!_redLabelLayer) {
+        _redLabelLayer = [CAShapeLayer layer];
+    }
+    return _redLabelLayer;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    CGRect rect = CGRectMake(0, 0, kImgViewWidth, kImgViewHeight);
+    UIBezierPath *path = [UIBezierPath bezierPathWithRect:rect];
     
-}
-- (void)viewDidAppear:(BOOL)animated {
-    [super viewDidAppear:animated];
-    self.shapLayer.frame = self.redLabel.bounds;
-    [self.redLabel.layer addSublayer:self.shapLayer];
+    self.shapLayer.path = path.CGPath;
+    self.topImgView.layer.mask = self.shapLayer;
+
+    [self.slider addTarget:self action:@selector(sliderValueChanged:) forControlEvents:UIControlEventValueChanged];
+    
+    [self setupRedLabelLayer];
 }
 
-- (IBAction)greenViewClicked:(id)sender {
-    
-    CGRect beginRect = CGRectMake(0, 66, 100, 100);
-    CGRect endRect = CGRectMake(100, 166, 200, 200);
-    
-    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"frame"];
-    animation.duration = 1.;
-    animation.fromValue = [NSValue valueWithCGRect:beginRect];
-    animation.toValue = [NSValue valueWithCGRect:endRect];
-    
-    
-    [self.greenView.layer addAnimation:animation forKey:nil];
+- (void)setupRedLabelLayer {
+    CGRect fromRect = CGRectMake(0, 0, 0, 30);
+    self.redLabelLayer.path = [UIBezierPath bezierPathWithRect:fromRect].CGPath;
+    self.redLabel.layer.mask = self.redLabelLayer;
 }
 
 
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    [self textProgressAnimation];
+- (void)testGetTime {
+//    NSLog(@"%ld", time(nil));
+//    NSLog(@"%lf", CFAbsoluteTimeGetCurrent());
+//    NSLog(@"%lf", CACurrentMediaTime());
+//
+    NSLog(@"%lf", self.shapLayer.beginTime);
+    NSLog(@"%lf", self.view.layer.beginTime);
 }
 
 
-- (void)textProgressAnimation {
+- (IBAction)startAnimation:(id)sender {
+    isAnimation = YES;
+    CGRect fromRect = CGRectMake(0, 0, kImgViewWidth, kImgViewHeight);
+    CGRect toRect = CGRectMake(0, 0, kImgViewWidth, 0);
+    UIBezierPath *fromPath = [UIBezierPath bezierPathWithRect:fromRect];
+    UIBezierPath *toPath = [UIBezierPath bezierPathWithRect:toRect];
     
-}
-
-- (void)animation_3 {
-    CAShapeLayer *layer = self.shapLayer;
+    [self.shapLayer removeAllAnimations];
+    self.shapLayer.speed = 1.;
+    self.shapLayer.path = fromPath.CGPath;
     
-    layer.fillColor = [UIColor redColor].CGColor;
-    layer.strokeColor = [UIColor greenColor].CGColor;
-    
-    CGRect rect = CGRectMake(20, 20, 200, 200);
-    UIBezierPath *beginPath = [UIBezierPath bezierPathWithOvalInRect:rect];
-    UIBezierPath *endPath = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(200, 200, 300, 200)];
+//    self.shapLayer.beginTime = [self.shapLayer convertTime:CACurrentMediaTime() fromLayer:nil] - 2;
+//    self.shapLayer.timeOffset = 1;
     
     CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"path"];
-    animation.duration = 2.;
-//    animation.fillMode = kCAFillModeForwards;
+    animation.duration = 5.;
+    animation.speed = 1;
+    animation.fromValue = (__bridge id _Nullable)(fromPath.CGPath);
+    animation.toValue = (__bridge id _Nullable)(toPath.CGPath);
     
-//    animation.removedOnCompletion = YES;
-    animation.fromValue = (__bridge id _Nullable)(beginPath.CGPath);
-    animation.toValue = (__bridge id _Nullable)(endPath.CGPath);
-    
-    [layer addAnimation:animation forKey:nil];
+    [self.shapLayer addAnimation:animation forKey:animation.keyPath];
+    self.shapLayer.path = toPath.CGPath;
 }
 
-- (void)animation_2 {
-    [CATransaction begin];
-    [CATransaction setDisableActions:NO]; // NO有动画，YES：关闭动画
-    [CATransaction setAnimationDuration:1];
-    self.shapLayer.position = CGPointMake(300, 300);
-    [CATransaction commit];
+- (IBAction)pauseAnimation:(id)sender {
+    isAnimation = NO;
+    CFTimeInterval currentTime = [self.shapLayer convertTime:CACurrentMediaTime() fromLayer:nil];
+    self.shapLayer.timeOffset = currentTime;
+    self.shapLayer.speed = 0;
+    
 }
 
-- (void)animation_1 {
-    CGRect beginRect = CGRectMake(0, 66, 100, 100);
-    CGRect endRect = CGRectMake(100, 166, 200, 200);
+- (IBAction)resumeAnimation:(id)sender {
+    if (isAnimation) {
+        return;
+    }
+    isAnimation = YES;
+    self.shapLayer.speed = 1.;
+    CFTimeInterval timeOffset = self.shapLayer.timeOffset;
+    self.shapLayer.timeOffset = 0;
+    self.shapLayer.beginTime = 0;
     
-    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"position"];
-    animation.duration = 1.;
-    animation.fromValue = [NSValue valueWithCGPoint:CGPointMake(10, 70)];
-    animation.toValue = [NSValue valueWithCGPoint:CGPointMake(300, 200)];
+    CFTimeInterval timeSincePause = [self.shapLayer convertTime:CACurrentMediaTime() fromLayer:nil] - timeOffset;
+    self.shapLayer.beginTime = timeSincePause;
     
-    [self.shapLayer addAnimation:animation forKey:nil];
+    
+}
+
+#pragma -mark
+#pragma -mark layer动画
+- (void)sliderValueChanged:(UISlider *)sender {
+    static CGFloat oldValue = 0;
+    CGFloat labelW = [UIScreen mainScreen].bounds.size.width - 20;
+    
+    CGRect fromRect = CGRectMake(0, 0, labelW * oldValue, 30);
+    CGRect toRect = CGRectMake(0, 0, labelW * sender.value, 30);
+    oldValue = sender.value;
+    
+    [self.redLabelLayer removeAllAnimations];
+    
+    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"path"];
+    animation.duration = 0.3;
+    animation.speed = 1;
+    animation.fromValue = (id)[UIBezierPath bezierPathWithRect:fromRect].CGPath;
+    animation.toValue   = (id)[UIBezierPath bezierPathWithRect:toRect].CGPath;
+
+    [self.redLabelLayer addAnimation:animation forKey:animation.keyPath];
+    self.redLabelLayer.path = [UIBezierPath bezierPathWithRect:toRect].CGPath;
+    
 }
 
 @end
+
+
+
+
+
 
 
 
