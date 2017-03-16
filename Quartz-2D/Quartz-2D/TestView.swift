9e5bf0e1730a4a1d495649cf189453e69f4ca3bd
吴贞利 <wuzhenli@kongfz.com>
2017-03-16 15:32:29 +0800

    1、Quartz 2D练习

--- Quartz-2D/Quartz-2D/TestView.swift
+++ Quartz-2D/Quartz-2D/TestView.swift
@@ -7,11 +7,40 @@
 //
 
 import UIKit
+import QuartzCore
+import CoreGraphics
+
 
 class TestView: UIView {
 
     override func draw(_ rect: CGRect) {
-        drawTest_1()
+        drawTest_3()
+    }
+    
+    func drawTest_4() {
+        
+    }
+    
+    
+    func drawTest_3()  {
+        let con = UIGraphicsGetCurrentContext()!
+        con.move(to: CGPoint(x: 10, y: 10))
+        con.addLine(to: CGPoint(x: 100, y: 10))
+        con.addLine(to: CGPoint(x: 60, y: 33))
+        con.addLine(to: CGPoint(x: 10, y: 10))
+        con.setStrokeColor(UIColor.red.cgColor)
+        con.setLineWidth(1.2)
+        
+        con.strokePath()
+    }
+    
+    func drawTest_2() {
+        let con = UIGraphicsGetCurrentContext()
+        con?.addEllipse(in: CGRect(x: 10, y: 5, width: 59, height: 33))
+        con?.setFillColor(UIColor.red.cgColor)
+        con?.setStrokeColor(UIColor.green.cgColor)
+        
+        con?.fillPath()
     }
     
     func drawTest_1() {
--- Quartz-2D/Quartz-2D/ViewController.swift
+++ Quartz-2D/Quartz-2D/ViewController.swift
@@ -7,14 +7,30 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    lazy var imgView = { () -> UIImageView in 
+        let imgV = UIImageView()
+        imgV.backgroundColor = UIColor.groupTableViewBackground
+
+        return imgV
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         addQuartzView_1()
+        
+        view.addSubview(imgView)
+        imgView.image = drawPic()
+        imgView.snp.makeConstraints { (make) in
+            make.center.equalTo(view)
+            make.width.height.equalTo(100)
+        }
     }
+    
 
 
 }
@@ -27,5 +43,40 @@ extension ViewController {
         v.frame = rect
         view.addSubview(v)
     }
+    func drawPic() -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 100, height:100), false, UIScreen.main.scale)
+        
+        let path = UIBezierPath(ovalIn: CGRect(x: 0, y: 0, width: 100, height: 100))
+        UIColor.green.setFill()
+        path.fill()
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return image!
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
