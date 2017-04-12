83a7765e0c08f479d1fb2e93ead4cccffecb64fc
吴贞利 <wuzhenli@kongfz.com>
2017-04-12 09:48:46 +0800

    1、KVC不一定触发KVO

---
+++ 多种测试/Demo_FrameWork/Class/SubPage/KVO_Person.m
@@ -0,0 +1,13 @@
+//
+//  KVO_Person.m
+//  多种测试
+//
+//  Created by kfz on 2017/4/12.
+//  Copyright © 2017年 Jaly. All rights reserved.
+//
+
+#import "KVO_Person.h"
+
+@implementation KVO_Person
+
+@end
